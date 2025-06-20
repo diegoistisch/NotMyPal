bc3129fe6132596151c95ebf0da61b3df1346776
Diego Traxler <e12119878@student.tuwien.ac.at>
2025-06-20 03:26:55 +0200

    Neue Datei capacitor.config.ts hinzugefügt, um die Konfiguration für die Capacitor-App 'notmypal' festzulegen.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.notmypal',
+  appName: 'notmypal',
+  webDir: 'build'
+};
+
+export default config;
